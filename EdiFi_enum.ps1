fa2cafcc42527ea0d16ce46bfec65e1b79e6bea2
Tiger <107926734+pf-tiger@users.noreply.github.com>
2023-02-13 13:39:25 +0900

    Create EdiFi_enum.ps1
    Added a script file

---
+++ EdiFi_enum.ps1
@@ -0,0 +1,28 @@
+# This script is for enumerating wifi password on the device, and exporting the result to a text file.
+# Show registered Wi-Fi AP SSIDs to external files
+$aup = "    All User Profile     : "
+$kc  = "    Key Content            : "
+
+netsh wlan show profiles | find " : "|Out-File ./SSIDs.txt 
+# clean up the data and delete unecessary parts.
+$SSID_list = Get-Content ./SSIDs.txt | ForEach-Object {$_ -replace $aup, ""}
+$SSID_list | Out-File ./SSIDs.txt
+# Get-Content ./SSIDs.txt
+
+# Function to provide a password: argument: SSID
+$SSIDs = Get-Content ./SSIDs.txt
+function show_pw {
+    param (
+        $temp_SSID 
+    )
+    netsh wlan show profile name=$temp_SSID key=clear | find "Key Content"
+}
+
+# repeat show_pw and append to a csv file
+# read each line of ./SSIDs.txt and use it as argument: SSID'
+
+ForEach ($SSID in $SSIDs) {
+    $pw = show_pw $SSID
+    $pair = $SSID + "," + $pw -replace $kc, "" 
+    $pair | Add-Content ./wlanpass.csv
+}
